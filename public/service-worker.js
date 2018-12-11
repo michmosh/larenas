a0a13f1e51b2f0aac132be3d358a0b3acbfd5c36
moshiko <michmosh1@gmail.com>
2018-12-12 01:52:28 +0200

    main background image

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cea8db072efdedf0bc0fc1c2d81466a4.js"
+  "/precache-manifest.92827c3bf770de1fa7e9096480d5b636.js"
 );
 
 workbox.clientsClaim();
