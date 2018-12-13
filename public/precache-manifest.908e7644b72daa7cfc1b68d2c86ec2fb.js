445c906f122d6aac8132ba7fa768ff5a6adf98aa
moshiko <michmosh1@gmail.com>
2018-12-13 13:14:30 +0200

    css + icon + code cleaning

---
+++ public/precache-manifest.908e7644b72daa7cfc1b68d2c86ec2fb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "e3357bfa5131c7f451c2",
+    "url": "/static/css/main.cc9b5213.chunk.css"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "96c7d99b682c39bd93e6",
+    "url": "/static/js/1.96c7d99b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e3357bfa5131c7f451c2",
+    "url": "/static/js/main.e3357bfa.chunk.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "96c7d99b682c39bd93e6",
+    "url": "/static/css/1.9bb9ae1f.chunk.css"
+  },
+  {
+    "revision": "22ce2bfbc1e9ffc0b8344bc6c796f955",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3777086ed5b53082804e469a7d238063.js"
+  "/precache-manifest.908e7644b72daa7cfc1b68d2c86ec2fb.js"
 );
 
 workbox.clientsClaim();
