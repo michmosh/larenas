ddb1e53adc444001e26be461190e446f978e63e7
Ubuntu <ubuntu@ip-172-31-19-126.us-west-2.compute.internal>
2020-03-24 11:38:48 +0000

    db change

--- database/db.js
+++ database/db.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dburl = 'mongodb://localhost:27017/hotel';
-const mLabUrl = 'mongodb://@ds123834.mlab.com:23834/hotel'
+const mLabUrl = 'mongodb://michmosh:ceim5193@ds123834.mlab.com:23834/hotel';
 
-mongoose.connect(mLabUrl,{ 
-    useNewUrlParser: true ,
-    auth:{
-        user : 'michmosh',
-        password: 'ceim5193'
-    }
-    }
-   );
+mongoose.connect(mLabUrl);
 const db = mongoose.connection;
 db.on('error' ,console.error.bind(console, 'MongoDB connection error:'));
 
